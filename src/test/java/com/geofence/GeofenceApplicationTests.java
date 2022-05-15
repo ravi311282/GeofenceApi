936da2d899276a24f90855d8a5680a8d95ce9b14
RaviReddy <lravikum456@gmail.com>
2022-05-15 23:34:37 +0100

    Required Project Setup for Geofence API development.

---
+++ src/test/java/com/geofence/GeofenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geofence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeofenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
